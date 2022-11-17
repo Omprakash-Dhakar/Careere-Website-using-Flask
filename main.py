de693d801003c24e2ee70b7c48f3cc033e3f9ef9
OMPRAKASH DHAKAR <115010509+Omprakash-Dhakar@users.noreply.github.com>
2022-11-17 11:07:48 +0000

    create an API route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
@@ -30,10 +30,15 @@ JOBS=[
 ]
 
 @app.route("/")
-def hello_world():
+def hello_google():
   return render_template("home.html",
                         jobs=JOBS,
                         company_name="Google")
 
+@app.route("/api/career")
+def list_jobs():
+  return jsonify(JOBS)
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
+  
